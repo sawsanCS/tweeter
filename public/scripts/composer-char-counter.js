1799816ec703f4d720396e2bb3872496c323e46f
sawsanCS <sawsan_sli3i@live.fr>
2020-12-15 15:06:14 +0000

    adjusted the code so that it turns red when invalid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     $("#tweet-text").on('keypress', function() {
         
         document.getElementById('counter').innerHTML = parseInt( (document.getElementById('counter').innerText)- 1);//The this keyword is a reference to the button
+        if (document.getElementById('counter').innerHTML < 0) {
+            $('.counter').css('color', 'red');
+        }
       });
   });
   
