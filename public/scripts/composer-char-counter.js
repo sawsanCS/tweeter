a554fcbef44ba65ddbcd910b63bcd759cd21bc4a
sawsanCS <sawsan_sli3i@live.fr>
2020-12-23 02:01:00 +0000

    changed my counter code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,31 @@
 $(document).ready(function () {
     // --- our code goes here ---
-    // $('form').on('submit', (event)=> {
-    //     event.preventDefault();
-    // });
-    $("#tweet-text").on('keypress', function() {
+    $("#tweet-text").on('input', function () {
         
-        document.getElementById('counter').innerHTML = parseInt( (document.getElementById('counter').innerText)- 1);//The this keyword is a reference to the button
-        if (document.getElementById('counter').innerHTML < 0) {
-            $('.counter').css('color', 'red');
-        }
-      });
-      $('.posted-tweets').on('mouseover', function(){
-          $('.link').css('visibility', 'visible');
-      });
-      $('.posted-tweets').on('mouseout', function(){
-        $('.link').css('visibility', 'hidden');
-    });
+        let tweetLength = $(this).val().length;
      
-  });
+      
+        $('#counter').text(140 - tweetLength);
+        if (tweetLength > 140) {
+            $('#counter').css('color', 'red');
+        } else {
+            $('#counter').css('color', 'black');
+        }
+    });
+
   
+
+        //     document.getElementById('counter').innerHTML = parseInt( (document.getElementById('counter').innerText)- 1);//The this keyword is a reference to the button
+        //     if (document.getElementById('counter').innerHTML < 0) {
+        //         $('.counter').css('color', 'red');
+        //     }
+        //   });
+        $('.posted-tweets').on('mouseover', function () {
+            $('.link').css('visibility', 'visible');
+        });
+        $('.posted-tweets').on('mouseout', function () {
+            $('.link').css('visibility', 'hidden');
+        });
+
+    });
+
